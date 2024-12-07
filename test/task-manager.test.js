3c9e773263ba8d6f8e9da6cad3957400e2121dd3
Erin Walter <91741075+erinmwalter@users.noreply.github.com>
2024-12-06 20:24:58 -0500

    Initial commit

---
+++ test/task-manager.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const TaskManager = require('../lib/task-manager-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task-manager-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new TaskManager.TaskManagerStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
